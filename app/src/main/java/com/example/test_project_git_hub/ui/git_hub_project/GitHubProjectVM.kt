aae6612368cdb6ad184d88a3a9f5fd7612a740e7
andrewmytsenko <mytsenkoandrew@gmail.com>
2020-11-02 14:31:03 +0200

    added navigation

--- app/src/main/java/com/example/test_project_git_hub/ui/git_hub_project/GitHubProjectVM.kt
+++ app/src/main/java/com/example/test_project_git_hub/ui/git_hub_project/GitHubProjectVM.kt
@@ -1,7 +1,6 @@
 package com.example.test_project_git_hub.ui.git_hub_project
 
 import androidx.lifecycle.ViewModel
-import com.example.test_project_git_hub.base_controler.BaseViewModel
 import javax.inject.Inject
 
 class GitHubProjectVM @Inject constructor() : ViewModel() {
