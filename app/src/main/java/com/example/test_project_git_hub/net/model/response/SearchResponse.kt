34b16e48e9719c04d1e0a7c45a300d53a9a898fa
andrewmytsenko <mytsenkoandrew@gmail.com>
2020-11-03 14:44:24 +0200

    change logic call api

--- app/src/main/java/com/example/test_project_git_hub/net/model/response/SearchResponse.kt
+++ app/src/main/java/com/example/test_project_git_hub/net/model/response/SearchResponse.kt
@@ -2,12 +2,13 @@ package com.example.test_project_git_hub.net.model.response
 
 
 import com.google.gson.annotations.SerializedName
+import java.util.ArrayList
 
 data class SearchResponse(
     @SerializedName("incomplete_results")
     val incompleteResults: Boolean,
     @SerializedName("items")
-    val projects: List<Project>,
+    val projects: ArrayList<Project>,
     @SerializedName("total_count")
     val totalCount: Int
 )
--- app/src/main/java/com/example/test_project_git_hub/ui/git_hub_project/GitHubProjectVM.kt
+++ app/src/main/java/com/example/test_project_git_hub/ui/git_hub_project/GitHubProjectVM.kt
@@ -1,12 +1,9 @@
 package com.example.test_project_git_hub.ui.git_hub_project
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.test_project_git_hub.net.api.Api
 import com.example.test_project_git_hub.net.model.response.SearchResponse
-import com.google.gson.Gson
-import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
@@ -15,35 +12,38 @@ import javax.inject.Inject
 
 class GitHubProjectVM @Inject constructor(private val api: Api) : ViewModel() {
     private var disposable: Disposable? = null
-     val foundProjectResponse = MutableLiveData<SearchResponse>()
+    val foundProjectResponse = MutableLiveData<SearchResponse>()
+    val foundProjectResponseError = MutableLiveData<String>()
+    private val PAGE_1 = "1"
+    private val PAGE_2 = "2"
 
-    fun validateTransactionFactor(q: String, countPages: Int) {
-        val s = Observable.range(1, countPages)
-            .map { page ->
-                api.getRepositories(
-                    q = q,
-                    page = page.toString()
-                )
-            }
+    fun validateTransactionFactor(q: String) {
+        disposable?.dispose()
+        val request =
+            api.getRepositories(
+                q = q,
+                page = PAGE_1
+            )
 
-
-        disposable = Observable.switchOnNext(s)
+        val requestTwo =
+            api.getRepositories(
+                q = q,
+                page = PAGE_2
+            )
+        disposable = request
+            .mergeWith(requestTwo)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({ response ->
                 if (response.isSuccessful) {
                     response.body()?.run {
-                        foundProjectResponse
-                            Log.d("sad0", Gson().toJson(this))
+                        foundProjectResponse.postValue(this)
                     }
+                } else {
+                    foundProjectResponseError.postValue(response.message())
                 }
-
             }, {
-
+                foundProjectResponseError.postValue(it.message)
             })
-
-
     }
-
-
 }
